7ba6861d91e32845af7d4b9bd2a220811cb05cd5
Ruhull Alam <Ruhull.alam@outlook.com>
2017-07-28 15:43:05 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const request = require('request')
+const yargs = require('yargs')
+
+const aOptions = {
+  demand : true,
+  alias : "address",
+  describe : "Address to fetch weather for",
+  string : true
+}
+
+const argv = yargs
+  .options({
+    a : aOptions
+  })
+  .help()
+  .alias('help', 'h')
+  .argv
+
+const reqOptions = {
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
+  json: true
+}
+
+request(reqOptions ,
+  (error, response, body) => {
+    if (error) {
+      console.log(error.code)
+    } else {
+      console.log(`Searching for : ${body.results[0].formatted_address}`)
+      console.log("Latitude : " + body.results[0].geometry.location.lat)
+      console.log("Longitude : " + body.results[0].geometry.location.lng)
+    }
+})
