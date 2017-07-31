65daad9f5e666777109ac6ab7087158a11e19b33
Ruhull Alam <Ruhull.alam@outlook.com>
2017-07-31 12:27:29 +0100

    further error handling added

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ const reqOptions = {
 request(reqOptions ,
   (error, response, body) => {
     if (error) {
-      console.log(error.code)
-    } else {
+      console.log(error.code + " Unable to connect to Google servers")
+    } else if (body.status === "ZERO_RESULTS") {
+      console.log('No results found')
+    } else if (body.status === "OK") {
       console.log(`Searching for : ${body.results[0].formatted_address}`)
       console.log("Latitude : " + body.results[0].geometry.location.lat)
       console.log("Longitude : " + body.results[0].geometry.location.lng)
